87340d204f3b3be9fb21553989007a5cfba3ae81
Maric Brandi <theoneandonlymandarr@gmail.com>
2020-12-06 22:36:29 -0500

    add controller files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { getAllAuthors, getAuthorByIdWithNovelsAndNovelGenres } = require('./controllers/authors.js')
-const { getAllGenres } = require('./controllers/genres.js')
+const { getAllGenres, getGenreByIdWithNovelsAndNovelAuthors } = require('./controllers/genres.js')
+const { getAllNovelsWithAuthorsAndGenres, getNovelByIdWithAuthorsAndGenres } = require('./controllers/novels.js')
 
 const app = express()
 
@@ -9,6 +10,12 @@ app.get('/authors', getAllAuthors)
 app.get('/authors/:id', getAuthorByIdWithNovelsAndNovelGenres)
 
 app.get('/genres', getAllGenres)
+
+app.get('/genres/:id', getGenreByIdWithNovelsAndNovelAuthors)
+
+app.get('/novels', getAllNovelsWithAuthorsAndGenres)
+
+app.get('/novels/:id', getNovelByIdWithAuthorsAndGenres)
 
 app.listen(1537, () => {
   console.log('Listening on port 1537...') // eslint-disable-line no-console
